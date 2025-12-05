46afa9bfc891a2390bce5384f0320aed22541cae
Chupakhin Arthur <rezident.artom@yandex.ru>
2025-12-05 22:11:13 +0000

    Update file run.sh

--- insmapreducetask1/run.sh
+++ insmapreducetask1/run.sh
@@ -3,7 +3,7 @@
 OUT_DIR=/user/instruments2025a07/output_$(date +%s%N)
 NUM_REDUCERS=4
 
-hdfs dfs -rm -r -skipTrash ${OUT_DIR} 
+hdfs dfs -rm -r -skipTrash ${OUT_DIR} > /dev/nul
 
 yarn jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
     -D mapred.job.name="Mixing IDs_Arthur_Chupakhin" \
@@ -12,6 +12,6 @@ yarn jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
     -mapper mapper.py \
     -reducer reducer.py \
     -input /data/ids \
-    -output ${OUT_DIR} 
+    -output ${OUT_DIR} > /dev/nul
 
-hdfs dfs -cat ${OUT_DIR}/part-* | head -n 50
+hdfs dfs -cat ${OUT_DIR}/part-00000 | head -n 50
