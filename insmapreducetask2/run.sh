ae8cc3e8040d53be153872a91eaa709477c0d1e1
Chupakhin Arthur <rezident.artom@yandex.ru>
2025-12-12 13:07:40 +0000

    Update file run.sh

--- insmapreducetask2/run.sh
+++ insmapreducetask2/run.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
 STREAMING_JAR="/opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar"
+INPUT_DATA="/data/wiki/en_articles" 
+OUTPUT_JOB="/user/$(whoami)/bigrams_counts"
 
-
-INPUT_DATA="/data/wiki/en_articles_part"  # Используем семпл, для полных данных: /data/wiki/en_articles
-OUTPUT_JOB1="/user/$(whoami)/bigrams_counts"
-
-hdfs dfs -rm -r -skipTrash ${OUTPUT_JOB1} 2>/dev/null
+hdfs dfs -rm -r -skipTrash ${OUTPUT_JOB} 2>/dev/null
 
 echo "=== Запуск Job 1: Подсчёт биграмм по документам ==="
 hadoop jar ${STREAMING_JAR} \
     -D mapreduce.job.name="Bigrams Count" \
     -D mapreduce.job.reduces=1 \
     -input ${INPUT_DATA} \
-    -output ${OUTPUT_JOB1} \
+    -output ${OUTPUT_JOB} \
     -mapper mapper.py \
     -reducer reducer.py \
     -file mapper.py \
     -file reducer.py > /dev/null
 
-if [ $? -ne 0 ]; then
-    echo "Ошибка в Job 1"
-    exit 1
-fi
-
-echo "=== Job 1 завершён успешно ==="
-echo ""
 
 hdfs dfs -cat /user/$(whoami)/bigrams_counts/part-* | \
     sort -t$'\t' -k2 -rn | \
