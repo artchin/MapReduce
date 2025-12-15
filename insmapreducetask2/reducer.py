476539725d45a0212249782f4a0c2cd8c9f772ad
Chupakhin Arthur <rezident.artom@yandex.ru>
2025-12-15 10:43:52 +0000

    Update file reducer.py

--- insmapreducetask2/reducer.py
+++ insmapreducetask2/reducer.py
@@ -2,28 +2,27 @@
 import sys
 
 current_bigram = None
-doc_ids = 0
+count = 0 
 
 for line in sys.stdin:
     line = line.strip()
     if not line:
         continue
-
+    
     try:
         bigram, doc_id = line.split('\t')
     except ValueError:
         continue
-
+    
     if current_bigram == bigram:
-        doc_ids += 1
+        count += 1 
     else:
         if current_bigram is not None:
-            count = len(doc_ids)
             print(f"{current_bigram}\t{count}")
-
+        
         current_bigram = bigram
-        doc_ids = 1
+        count = 1  
 
 if current_bigram is not None:
-    print(f"{current_bigram}\t{doc_ids}")
+    print(f"{current_bigram}\t{count}")
 
